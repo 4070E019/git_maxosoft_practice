98760618f39c1bbd7b429bfc5106a239b7565e2a
4070E019 <43432169+4070E019@users.noreply.github.com>
2021-07-26 15:14:56 +0800

    Add files via upload

---
+++ 109_ksu_maxosoft_intern/Alexander/0726/info.vue
@@ -0,0 +1,26 @@
+<template>
+   <div class = "card">
+       <div class="card-header">
+        Bar
+        </div>
+        <div class="card-body">
+            <bar :data="data" :option="option"> </bar>
+      </div>
+     </div>
+</template>
+
+<script>
+import bar from './bar.vue';
+
+
+export default{
+       name:'info',
+       props:[
+           'data',
+           'option'
+       ],
+       components:{
+           bar
+       }
+}
+</script>
