1aaafa367879e3d9c3db146e55a72df990b19edf
4070E019 <43432169+4070E019@users.noreply.github.com>
2021-08-04 17:29:26 +0800

    Add files via upload

---
+++ 109_ksu_maxosoft_intern/Alexander/0804/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app" >
+        
+        {{ info }}
+      </div>
+
+    </template>
+
+<script>
+    import axios from "axios";
+    
+    export default {
+      name: 'app',
+      data () {
+        return {
+          loading: false,
+         info: null
+        }
+      },
+      mounted () {
+        this.loading = true;
+        axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+      }
+    }
+    </script>
