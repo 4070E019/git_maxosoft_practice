10312a8f475656c8b8425fde52b1931d8f055125
4070E019 <43432169+4070E019@users.noreply.github.com>
2021-08-11 17:18:08 +0800

    Add files via upload

---
+++ 109_ksu_maxosoft_intern/Alexander/0810/App.vue
@@ -0,0 +1,59 @@
+<template>
+
+  <div id="app" class="container">
+   
+    
+    <p v-if="loading">Loading...</p>
+    <div v-else>
+      <h3 class="heading">Users</h3>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">weight </th>
+          
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="products in info" v-bind:key="products">
+            <td>{{ products.id }}</td>
+            <td>{{ products.weight }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+    import axios from "axios";
+    
+    export default {
+      name: 'app',
+      data () {
+        return {
+          loading: false,
+         info: null,
+         errored: false,
+        }
+      },
+      mounted () {
+        this.loading = true;
+        axios
+        .get('https://yjserp.maxosoft.com/api/final_demo')
+      .then(response => (this.info = response.data.products))
+       .catch(error => console.log(error))
+      .finally(() => this.loading = false)
+      }
+    }
+    </script>
+
+    <style>
+#app {
+  text-align: center;
+  margin-top: 50px;
+}
+.heading {
+  margin-bottom: 30px;
+}
+</style>
